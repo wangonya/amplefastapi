2b44636d0a7a92deff488fcc6c3bfea15acc15e1
kelvin <kelvin@jenga-agency.com>
2022-02-12 18:05:45 +0300

    progress

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="amplefastapi",
-    version="0.0.1",
+    version="0.0.2",
     author="Kelvin Wangonya",
     author_email="kwangonya@gmail.com",
     description="Ample Analytics middleware package for FastApi",
